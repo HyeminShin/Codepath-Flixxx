7e5e9cf3541cd96819ff8fa91ee4a9eef6050670
HyeminShin <57362464+HyeminShin@users.noreply.github.com>
2022-03-05 03:08:59 -0500

    Flixxx
    including unit1 and unit 2 work

---
+++ Flixxx/MovieGridCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieGridCell.swift
+//  Flixxx
+//
+//  Created by Hyemin Shin on 3/5/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    
+}
